fd1dd2da93acb5361d757a56f5fea7f1a6b02eca
Abdalrhman M. Alkady <Phoenix-H22@users.noreply.github.com>
2020-12-26 16:10:36 +0200

    awd

--- js/custom.js
+++ js/custom.js
@@ -370,3 +370,19 @@ $(function () {
 
 
 });
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
